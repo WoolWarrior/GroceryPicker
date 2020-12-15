6d5e06b685c1aad4ac17850f136f617b68066222
Wool Warrior <awoolwarrior@gmail.com>
2020-12-15 22:17:41 +0000

    Supported add cart to Sainsburys

--- addToCart.js
+++ addToCart.js
@@ -1,10 +1,7 @@
+let host = window.location.host; 
 let url = window.location.href;
 console.log({url});
-
-// if the item is OOS, isOutOfStock is 1.
-let isOutOfStock = document.getElementsByClassName('bop-outOfStock__text').length;
 let altertText = "Nothing happen!";
-
 
 // function to download a text file
 const downloadToFile = (content, filename, contentType) => {
@@ -17,24 +14,69 @@ const downloadToFile = (content, filename, contentType) => {
   URL.revokeObjectURL(a.href);
 };
 
-if (isOutOfStock !== 1) {
-  
-  // if the item is in cart already, hasInCart is 2
-  let hasInCart = document.getElementsByClassName('gn-button gn-button--buy basketControls__changeQuantity--plus').length;
-  if (hasInCart !== 2){
-    // The item is no in the cart yet.
-    document.getElementsByClassName('gn-button gn-button--buy basketControls__add')[0].click();
-    altertText = "1 This item is added to your cart!";
-  } else {
-    // The item is in the cart. 
-    let numberInCart = document.getElementsByName('quantity')[0].value;
-  	document.getElementsByClassName('gn-button gn-button--buy basketControls__changeQuantity--plus')[0].click();
-  	altertText = "2 This item is added to your cart! You have " + (++numberInCart) + " in your cart.";
-  }
-} else {
-  altertText = "3 This item is out of stock.";
-  let itemName = document.getElementsByClassName('bop-title')[0].getElementsByTagName('h2')[0].innerText;
-  itemName += ' ' + url;
-  downloadToFile(itemName, 'Out-of-stock-item.txt', 'text/plain');
+if (host === "groceries.morrisons.com") {
+  addToCartMorrisons();
+  alert(altertText);
+} 
+
+if (host === "www.sainsburys.co.uk") {
+  setTimeout(() => {
+    addToCartSainsburys(); 
+    alert(altertText);
+  }, 5000);
 }
-alert(altertText);
+
+function addToCartMorrisons() {
+  // if the item is OOS, isOutOfStock is 1.
+  let isOutOfStock = document.getElementsByClassName('bop-outOfStock__text').length;
+
+  if (isOutOfStock !== 1) {
+    
+    // if the item is in cart already, hasInCart is 2
+    let hasInCart = document.getElementsByClassName('gn-button gn-button--buy basketControls__changeQuantity--plus').length;
+    if (hasInCart !== 2){
+      // The item is not in the cart yet.
+      document.getElementsByClassName('gn-button gn-button--buy basketControls__add')[0].click();
+      altertText = "1 This item is added to your cart!";
+    } else {
+      // The item is in the cart. 
+      let numberInCart = document.getElementsByName('quantity')[0].value;
+      document.getElementsByClassName('gn-button gn-button--buy basketControls__changeQuantity--plus')[0].click();
+      altertText = "2 This item is added to your cart! You have " + (++numberInCart) + " in your cart.";
+    }
+  } else {
+    altertText = "3 This item is out of stock.";
+    let itemName = document.getElementsByClassName('bop-title')[0].getElementsByTagName('h2')[0].innerText;
+    itemName += ' ' + url;
+    downloadToFile(itemName, 'Out-of-stock-item.txt', 'text/plain');
+  }
+}
+
+function addToCartSainsburys() {
+
+  // if the item is OOS, isOutOfStock is 1.
+  let isOutOfStock = document.getElementsByClassName('pd__message').length;
+
+  if (isOutOfStock !== 1) {
+  
+    // if the item is in cart already, hasInCart is 1
+    let hasInCart = document.getElementsByClassName('ln-c-button ln-c-button--outlined pt-button__quantity').length;
+    if (hasInCart !== 1){
+      // The item is not in the cart yet.
+      document.getElementsByClassName('ln-c-button ln-c-button--filled pt__add-button')[0].click();
+      altertText = "1 This item is added to your cart!";
+    } else {
+      // The item is in the cart. 
+      let numberInCart = parseInt(document.getElementsByClassName('ln-c-button ln-c-button--outlined pt-button__quantity')[0].innerText);
+      document.getElementsByClassName('ln-c-button ln-c-button--filled pt-button__inc')[0].click();
+      altertText = "2 This item is added to your cart! You have " + (++numberInCart) + " in your cart.";
+    }
+  } else {
+    altertText = "3 This item is out of stock.";
+    let itemName = document.getElementsByClassName('pd__header')[0].innerText;
+    itemName += ' ' + url;
+    downloadToFile(itemName, 'Out-of-stock-item.txt', 'text/plain');
+  }
+}
+
+
