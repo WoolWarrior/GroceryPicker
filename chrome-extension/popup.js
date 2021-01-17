ac57efdbd88c6231c80a41c3eaca505cb4df2a36
Wool Warrior <awoolwarrior@gmail.com>
2021-01-17 16:07:29 +0000

    Packed uploading html to the extension'

--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -1,7 +1,6 @@
 'use strict';
 
-downloadCSV.onclick = function(element) {
-  let clicked = true;
+downloadCSV.onclick = function() {
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     chrome.tabs.sendMessage(tabs[0].id, {command: "clickToDownload"}, function(response) {
       console.log('response.result');
@@ -11,8 +10,10 @@ downloadCSV.onclick = function(element) {
 
 let checkbox1 = document.getElementById('checkbox1');
 
+let uploadButton = document.getElementById('openUpload');
+
 chrome.storage.sync.get('AddToCartEnabled', function(data) {
-  checkbox1.checked=data.AddToCartEnabled;
+  checkbox1.checked = data.AddToCartEnabled;
 });
 
 checkbox1.onchange = function() {
@@ -22,10 +23,17 @@ checkbox1.onchange = function() {
   });
 
   if(value) {
+    uploadButton.disabled = false;
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
       chrome.tabs.sendMessage(tabs[0].id, {command: "initAddToCart", AddToCartEnabled: value}, function(response) {
         console.log(response.result);
       });
     });
+  } else {
+    uploadButton.disabled = true;
   }
+}
+
+openUpload.onclick = function () {
+  chrome.tabs.create({url : "upload.html"}); 
 }
